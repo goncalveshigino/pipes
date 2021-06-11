face5f77e10aa304d0a065264b8876f0d0214475
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-11 22:03:54 +0100

    pipes

---
+++ src/app/pipes/mostrar.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MostrarPipe } from './mostrar.pipe';
+
+describe('MostrarPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MostrarPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
