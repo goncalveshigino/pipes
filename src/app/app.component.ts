bf51459ecf291e079793fb9e596bd32ac44f6919
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-11 20:18:21 +0100

    pipedate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 
-  name  : string = "Higino";
-
-  people: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  name      : string   = "Higino";
+  people    : string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI        : number   = Math.PI;
+  percentage: number   = 0.234; //Percent
+  salario   : number   = 1234; //CurrencyPipe
+  date      : Date = new Date();
   
-  PI: number = Math.PI;
-  percentage: number = 0.234; //Percent
-  salario: number = 1234; //CurrencyPipe
+  languege  : string = 'es';
+
+  promiseValue = new Promise<string>((resolve) => {
+     setTimeout(() => {
+       resolve('os dados chegaram')
+     }, 4500);
+  })
 
   heroe = {
     name: 'Logan',
